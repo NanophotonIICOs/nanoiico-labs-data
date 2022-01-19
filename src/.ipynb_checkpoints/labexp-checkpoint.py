943ebb13d6a429eb1ff8ec9fb01682e75957e685
Gabmtzz <gabmtzz27@gmail.com>
2022-01-19 14:53:09 -0600

    update 2

--- src/.ipynb_checkpoints/labexp-checkpoint.py
+++ src/.ipynb_checkpoints/labexp-checkpoint.py
@@ -61,11 +61,6 @@ class experiments:
         print(tabulate(self.ptable,self.headers,tablefmt="github",colalign=("center","left","center")))
 
 
-    def highdim(self):
-        self.totaldata = len(self.data)
-        self.listdim # this list holds of high dimension corresponds to each examperiment
-        for i in range(self.totaldata):
-            for j in self.data[i]:
-                checkdim = 
+ 
 
 
